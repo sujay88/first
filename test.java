2f87430635dac1e1e3b889778556abb52c135eb5
sujay88 <sujaysrivastava88@gmail.com>
2021-08-23 13:54:15 +0530

    Update test.java

--- test.java
+++ test.java
@@ -2,7 +2,7 @@
 public class test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// To get largest or second largest number
 int arr[] = {2,3,66,4,77,8,70};
 int g = 0;
 int s = 0;
